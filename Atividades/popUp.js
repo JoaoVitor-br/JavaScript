8ee4715777b59fa3d8a62958f3cec07bccaae92b
JoaoVitor-br <amorim.jao3@gmail.com>
2026-05-21 11:47:53 -0300

    .

---
+++ Atividades/popUp.js
@@ -0,0 +1,30 @@
+function abrirPopUp() {
+    document.getElementById("popUp").style.display = "block"
+}
+function fecharPopUp() {
+    document.getElementById("popUp").style.display = "none"
+}
+function abrirPopp() {
+    document.getElementById("popp").style.display = "block"
+}
+function fecharPopp() {
+    document.getElementById("popp").style.display = "none"
+}
+function abrirPopUpImg() {
+    document.getElementById("imgpopImg").style.display = "block"
+}
+function fecharPopUpImg() {
+    document.getElementById("imgpopImg").style.display = "none"
+}
+function abrirPopUpCimento() {
+    document.getElementById("popUpBloco").style.display = "block"
+}
+function fecharPopUpCimento() {
+    document.getElementById("popUpBloco").style.display = "none"
+}
+let verificado = document.getElementById("verificando").value
+function certificado (){
+    if(verificado == "murango"){
+        
+    }
+}
---
+++ Projeto/pop-up/script.js
@@ -0,0 +1,6 @@
+function abrirPopUp() {
+    document.getElementById("popUp").style.display = "block"
+}
+function fecharPopUp() {
+    document.getElementById("popUp").style.display = "none"
+}
